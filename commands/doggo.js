c31c7c8a09083d13cfa7c1d4cbe489e19238d2cd
shiny finder <30510670+shinyfinder@users.noreply.github.com>
2021-01-31 13:55:37 -0500

    add doggo

---
+++ commands/doggo.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: "doggo",
+	description: 'Dancing doggo.',
+	execute(message) {
+		message.channel.send('https://cdn.discordapp.com/attachments/756274705891197088/805280393308667904/anjingo.gif');
+	},
+};
